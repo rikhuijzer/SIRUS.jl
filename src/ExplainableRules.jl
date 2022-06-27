d7ab0ba8b0edca7fe645f38d8857d94b3d286ab9
rikhuijzer <rikhuijzer@pm.me>
2022-06-27 14:20:40 +0200

    Implement `_find_split`

--- src/ExplainableRules.jl
+++ src/ExplainableRules.jl
@@ -1,6 +1,7 @@
 module ExplainableRules
 
-# Float32 should be precise enough.
+using Random: AbstractRNG, default_rng
+
 const Float = Float32
 
 """
@@ -20,5 +21,61 @@ function gini(y::AbstractVector, classes::AbstractVector)
     end
     return sum(proportions .* (1 .- proportions))
 end
+
+struct Score{T}
+    feature::Int
+    cutpoint::T
+    value::Float
+end
+
+"""
+Return a view on all `y` for which the `comparison` holds in `X[:, feature]`.
+"""
+function _view_y(X, y, feature::Int, comparison, cutpoint)
+    indexes_in_region = comparison.(X[:, feature], cutpoint)
+    return view(y, indexes_in_region)
+end
+
+"Return the best split by picking the lowest score."
+function _best_split(scores::Vector{Score{T}})::Tuple{Int,T} where T
+    best_score_index = 0
+    best_score_value = 999.0f0
+    for i in 1:length(scores)
+        score = scores[i]
+        if score.value < best_score_value
+            best_score_index = i
+        end
+    end
+    best_score = scores[best_score_index]
+    return (best_score.feature, best_score.cutpoint)
+end
+
+"Return the split for which the gini index is minimized."
+function _find_split(X, y::AbstractVector, classes::AbstractVector)
+    best_score = Float(999)
+    best_score_feature = 0
+    best_score_cutpoint = eltype(X)(0)
+    for feature in 1:size(X, 2)
+        U = unique(X[:, feature])
+        for value in U
+            cutpoint = value
+            gini_left = gini(_view_y(X, y, feature, <, cutpoint), classes)
+            gini_right = gini(_view_y(X, y, feature, ≥, cutpoint), classes)
+            score = gini_left + gini_right
+            if score < best_score
+                best_score = score
+                best_score_feature = feature
+                best_score_cutpoint = cutpoint
+            end
+        end
+    end
+    return best_score_feature, best_score_cutpoint
+end
+
+function build_tree(rng::AbstractRNG, X, y::AbstractVector)
+    classes = unique(y)
+    
+end
+build_tree(X, y::AbstractVector) = build_tree(default_rng(), X, y)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,3 +5,9 @@ const ER = ExplainableRules
 const Float = ExplainableRules.Float
 
 @test ER.gini([1, 1, 1], [1]) == 0.0f0
+
+feature = 1
+@test collect(ER._view_y([1 2; 3 4], [1 2], feature, <, 2)) == [1]
+@test collect(ER._view_y([1 2; 3 4], [1 2], feature, >, 2)) == [2]
+
+@test ER._find_split([1 2; 3 4], vec([1 2]), vec([1 2])) == (1, 3)
