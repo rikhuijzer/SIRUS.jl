2734b9d4d24e215ac2b049d779bd6b813f6512d7
rikhuijzer <rikhuijzer@pm.me>
2023-06-08 12:30:46 +0200

    Fix test

--- test/forest.jl
+++ test/forest.jl
@@ -7,6 +7,7 @@ X = [1 2;
      3 4]
 y = [1, 2]
 
+y_view = Vector{eltype(y)}(undef, length(y))
 feature = 1
 @test collect(ST._view_y!(y_view, X[:, feature], [1 2], <, 2)) == [1]
 @test collect(ST._view_y!(y_view, X[:, feature], [1 2], >, 2)) == [2]
