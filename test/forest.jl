d553ba0802e4cfe6a1c1427b3e9644720529fe73
rikhuijzer <rikhuijzer@pm.me>
2023-06-08 12:28:56 +0200

    Fix test

--- test/forest.jl
+++ test/forest.jl
@@ -7,10 +7,9 @@ X = [1 2;
      3 4]
 y = [1, 2]
 
-y_view = Vector{eltype(y)}(undef, length(y))
 feature = 1
-@test collect(ST._view_y!(y_view, X, feature, [1 2], <, 2)) == [1]
-@test collect(ST._view_y!(y_view, X, feature, [1 2], >, 2)) == [2]
+@test collect(ST._view_y!(y_view, X[:, feature], [1 2], <, 2)) == [1]
+@test collect(ST._view_y!(y_view, X[:, feature], [1 2], >, 2)) == [2]
 
 @test ST._cutpoints([3, 1, 2], 2) == Float[1, 2]
 @test ST._cutpoints(1:9, 3) == Float[3, 5, 7]
