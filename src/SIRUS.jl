a28fcd272765545e9b9709a93d2adbc4eb7a98e3
rikhuijzer <rikhuijzer@pm.me>
2023-06-21 14:34:28 +0200

    Fix incompatible package versions

--- src/SIRUS.jl
+++ src/SIRUS.jl
@@ -7,12 +7,12 @@ using AbstractTrees: AbstractTrees, print_tree
 using CategoricalArrays: CategoricalValue, unwrap
 using InlineStrings: String255
 using LinearAlgebra: rank
+using MLJBase: mode
 using MLJLinearModels: MLJLinearModels, RidgeRegressor, glr
 using MLJModelInterface: UnivariateFinite, Probabilistic, fit
 using PrecompileSignatures: @precompile_signatures
 using Random: AbstractRNG, default_rng, seed!, shuffle
 using Statistics: mean, median
-using StatsBase: mode
 using Tables: Tables, matrix
 
 export feature_names, directions, satisfies
